e8bcac7af71b93954b794e1603296821a8894e7b
dearFW <2559414906@qq.com>
2020-02-18 11:20:38 +0800

    javapro

--- src/com/company/test/History10.java
+++ src/com/company/test/History10.java
@@ -3,6 +3,7 @@ package com.company.test;
 /**
  * Created by heystephen on 2020/2/16.
  */
+//内部类
 public class History10 {
     public static void main(String[] args) {
         INTERface jia = new INTERface() {
@@ -12,6 +13,7 @@ public class History10 {
             }
         };
         System.out.println(method(10,2,jia));
+        //变形写法   直接用a b 运算
         System.out.println(method(12,20,(a,b)->{
             return a-b;
         }));
